1937f9468683969b0c1e72654e59d7e10af1b018
ayuksl <yuksel93@yahoo.com>
2020-12-19 18:25:09 +0000

    deg

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/folder/PetTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/folder/PetTest.java
@@ -1,5 +1,11 @@
+package org.springframework.samples.petclinic.customers.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
 public class PetTest {
-​
     @Test
     public void testGetName(){
         //Arrange
@@ -9,7 +15,6 @@ public class PetTest {
         //Assert
         assertEquals("Fluffy", pet.getName());
     }
-​
     @Test
     public void testGetOwner(){
         //Arrange
@@ -21,7 +26,6 @@ public class PetTest {
         //Assert
         assertEquals("Call", pet.getOwner().getFirstName());
     }
-
     @Test
     public void testBirthDate(){
         //Arrange
